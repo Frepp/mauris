1e5594353e656872e9c623cb8d0c5d1e2feaa167
Fredrik Peterson <fpeterson94@gmail.com>
2014-06-09 00:05:01 +0200

    Update index.tpl.php

--- src/CCIndex/index.tpl.php
+++ src/CCIndex/index.tpl.php
@@ -5,26 +5,27 @@
 if($link) {
 	echo "<div class='success'>Your PHP-version seems to be up to date. Great!</div>";	
 } else {
-	echo "<div class='error'>You're using an old PHP-version, please update the server.</div>";
+	echo "<div class='error'>You're using an old PHP-version, please update the server. Click <a href='https://github.com/Frepp/mauris/blob/master/README.md#php-version'>here</a> to see further information!</div>";
 }
 ?>
 <?php 
 if($link) {
 	echo "<div class='success'>Your .htaccess file seems to be correct. Great!</div>";	
 } else {
-	echo "<div class='error'>Your .htaccess file seems to be incorrect configurated.</div>";
+	echo "<div class='error'>Your .htaccess file seems to be incorrect configurated. Click <a href='https://github.com/Frepp/mauris/blob/master/README.md#configure-htaccess'>here</a> to see further information!</div>";
 }
 ?>
 <?php 
 if($permissions == "777") {
 	echo "<div class='success'>The permissions of site/data seems to be correct. Great!</div>";	
 } else {
-	echo "<div class='error'>The permissions of site/data are {$permissions}, they should be 777.</div>";
+	echo "<div class='error'>The permissions of site/data are {$permissions}, they should be 777. Click <a href='https://github.com/Frepp/mauris/blob/master/README.md#set-sitedata-permissions'>here</a> to see further information!</div>";
 }
 ?>
 
 <p>Problems? Please consult the <a href'https://github.com/Frepp/mauris/blob/master/README.md'>README</a> for further instructions.</p>
 
+<h2>Continue</h2>
 <p>Second, Mauris has some modules that need to be initialised. You can do this through a 
 controller. Point your browser to the following link.</p>
 <blockquote>
